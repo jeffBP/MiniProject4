84201b76b2a29df5356358cbe2c3b835784fadc5
FrancesDevanbu <frances.devanbu@students.olin.edu>
2017-03-01 21:55:00 -0500

    adding block and updating litforest

--- LitForest.py
+++ LitForest.py
@@ -9,6 +9,8 @@ class Game:
         self.size = self.width, self.height = 640, 480
         self.screen = None
         self.background = None
+        self.pxpos = 5
+        self.pypos = 7
 
     def on_init(self):
         pygame.init()
@@ -19,19 +21,32 @@ class Game:
         self.background = pygame.Surface(self.size)
         self.background= self.background.convert()
         self.background.fill((0,0,0))
+        #self.playerpos = self.playerstart
 
     def on_event(self,event):
         if event.type == pygame.QUIT:
             self.game_running= False
         if( pygame.key.get_pressed()[pygame.K_LEFT] != 0 ):
-            self.background.fill((45,0,0))
+            self.pxpos = self.pxpos -1
+        if( pygame.key.get_pressed()[pygame.K_RIGHT] != 0 ):
+            self.pxpos = self.pxpos +1
+        if( pygame.key.get_pressed()[pygame.K_DOWN] != 0 ):
+            self.pxpos = self.pxpos -1
+        if( pygame.key.get_pressed()[pygame.K_UP] != 0 ):
+            self.pxpos = self.pxpos +1
 
     def on_loop(self):
         pass
 
     def on_render(self):
         #self.background.blit(self.text)
+        point = pygame.image.load("ball.jpeg")
+        player = pygame.image.load("ball.jpeg")
+        matches = pygame.image.load("ball.jpeg")
         self.screen.blit(self.background,(0,0))
+        self.screen.blit(point,(400,0))
+        self.screen.blit(matches,(20,0))
+        self.screen.blit(player,(20*self.pxpos,20*self.pypos))
         pygame.display.flip()
 
     def on_cleanup(self):
